e4dae5e18d4f875de9d6f777b617172b3ed7040f
ActeonKay <135502692+ActeonKay@users.noreply.github.com>
2026-08-08 21:30:29 +0300

    Standardize the name scheme

--- functions/defaultOperators.js
+++ functions/defaultOperators.js
@@ -1,4 +1,4 @@
-export const OpCode = {
+export const OperatorCode = {
     ADD: 1, //binary +
     SUB: 2, //binary -
     MUL: 3, //binary *
@@ -29,13 +29,46 @@ export const OpCode = {
     EQEQ: 28, //binary (equality--assertion)
 }
 
-const OpAssociativity = {
+export const OperatorByLatex = {
+    "+": OperatorCode.ADD,
+    "-": OperatorCode.SUB,
+    "*": OperatorCode.MUL,
+    "\\cdot": OperatorCode.MUL,
+    "/": OperatorCode.DIV,
+    "^": OperatorCode.POW,
+    "<": OperatorCode.LT,
+    "\\leq": OperatorCode.LTE,
+    ">": OperatorCode.GT,
+    "\\geq": OperatorCode.GTE,
+    "=": OperatorCode.EQ,
+    "==": OperatorCode.EQEQ,
+    "\\neq": OperatorCode.NEQ,
+    "\\wedge": OperatorCode.AND,
+    "\\vee": OperatorCode.OR,
+    "\\oplus": OperatorCode.XOR,
+    "\\sim": OperatorCode.NOT,
+    "!": OperatorCode.FACT,
+    "u-": OperatorCode.NEG,
+    "\\cdot": OperatorCode.DPR,
+    "\\times": OperatorCode.CRP,
+    "\\backslash": OperatorCode.ATT,
+    "^n": OperatorCode.POWN,
+    "_": OperatorCode.SUBS,
+    "\\pm": OperatorCode.PM,
+    "\\%": OperatorCode.PCT,
+    "\\degree": OperatorCode.DEG,
+    "\\operatorname{abs}": OperatorCode.ABS,
+    "\\pm": OperatorCode.UNC,
+    "==": OperatorCode.EQEQ
+}
+
+export const OperatorAssociativity = {
     LEFT: 0,
     RIGHT: 1
 };
 
 //PEMDAS
-const OpPrecedence = {
+const OperatorPrecedence = {
     LOGICAL: -2,
     EQUALITY: -1,
     COMPARISON: 0,
@@ -45,65 +78,65 @@ const OpPrecedence = {
 }
 
 export const Operators = new Map([
-    [OpCode.ADD, { symbol: "+", precedence: 1, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.SUB, { symbol: "-", precedence: 1, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.MUL, { symbol: "*", precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.DIV, { symbol: "/", precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.POW, { symbol: "^", precedence: 3, associativity: OpAssociativity.RIGHT, arity: 2 }],
-    [OpCode.LT, { symbol: "<", precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.LTE, { symbol: "<=", precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.GT, { symbol: ">", precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.GTE, { symbol: ">=", precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.EQ, { symbol: "=", precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.EQEQ, { symbol: "==", precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.NEQ, { symbol: "!=", precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.AND, { symbol: "&&", precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.OR, { symbol: "||", precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.XOR, { symbol: "^^", precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.NOT, { symbol: "~", precedence: 5, associativity: OpAssociativity.RIGHT, arity: 1 }], //prefix unary (L)
-    [OpCode.FACT, { symbol: "!", precedence: 5, associativity: OpAssociativity.LEFT, arity: 1 }], //suffix unary (R)
-    [OpCode.NEG, { symbol: "u-", precedence: 3, associativity: OpAssociativity.RIGHT, arity: 1 }], //prefix unary (L)
-    [OpCode.DPR, { symbol: "dot", precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.CRP, { symbol: "times", precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.ATT, { symbol: "\\", precedence: 101, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.POWN, { symbol: "^n", precedence: 6, associativity: OpAssociativity.RIGHT, arity: 2 }], //for things like x^2!, the x^2 should come first as it is not x^{2!}
-    [OpCode.SUBS, { symbol: "_", precedence: 6, associativity: OpAssociativity.RIGHT, arity: 2 }],
-    [OpCode.PM, { symbol: "±", precedence: 1.5, associativity: OpAssociativity.LEFT, arity: 2 }],
-    [OpCode.PCT, { symbol: "%", precedence: 6, associativity: OpAssociativity.LEFT, arity: 1 }],
-    [OpCode.DEG, { symbol: "degree", precedence: 6, associativity: OpAssociativity.LEFT, arity: 1}],
-    [OpCode.ABS, { symbol: "abs", precedence: 6, associativity: OpAssociativity.LEFT, arity: 1}],
-    [OpCode.UNC, { symbol: "±", precedence: 1.5, associativity: OpAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.ADD, { symbol: "+", precedence: 1, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.SUB, { symbol: "-", precedence: 1, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.MUL, { symbol: "*", precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.DIV, { symbol: "/", precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.POW, { symbol: "^", precedence: 3, associativity: OperatorAssociativity.RIGHT, arity: 2 }],
+    [OperatorCode.LT, { symbol: "<", precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.LTE, { symbol: "<=", precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.GT, { symbol: ">", precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.GTE, { symbol: ">=", precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.EQ, { symbol: "=", precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.EQEQ, { symbol: "==", precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.NEQ, { symbol: "!=", precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.AND, { symbol: "&&", precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.OR, { symbol: "||", precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.XOR, { symbol: "^^", precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.NOT, { symbol: "~", precedence: 5, associativity: OperatorAssociativity.RIGHT, arity: 1 }], //prefix unary (L)
+    [OperatorCode.FACT, { symbol: "!", precedence: 5, associativity: OperatorAssociativity.LEFT, arity: 1 }], //suffix unary (R)
+    [OperatorCode.NEG, { symbol: "u-", precedence: 3, associativity: OperatorAssociativity.RIGHT, arity: 1 }], //prefix unary (L)
+    [OperatorCode.DPR, { symbol: "dot", precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.CRP, { symbol: "times", precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.ATT, { symbol: "\\", precedence: 101, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.POWN, { symbol: "^n", precedence: 6, associativity: OperatorAssociativity.RIGHT, arity: 2 }], //for things like x^2!, the x^2 should come first as it is not x^{2!}
+    [OperatorCode.SUBS, { symbol: "_", precedence: 6, associativity: OperatorAssociativity.RIGHT, arity: 2 }],
+    [OperatorCode.PM, { symbol: "±", precedence: 1.5, associativity: OperatorAssociativity.LEFT, arity: 2 }],
+    [OperatorCode.PCT, { symbol: "%", precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1 }],
+    [OperatorCode.DEG, { symbol: "degree", precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1}],
+    [OperatorCode.ABS, { symbol: "abs", precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1}],
+    [OperatorCode.UNC, { symbol: "±", precedence: 1.5, associativity: OperatorAssociativity.LEFT, arity: 2 }],
 ]);
 
-const OpInfo = {
-    "+": { code: OpCode.ADD, precedence: 1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "-": { code: OpCode.SUB, precedence: 1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "*": { code: OpCode.MUL, precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "cdot": { code: OpCode.MUL, precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "/": { code: OpCode.DIV, precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "^": { code: OpCode.POW, precedence: 3, associativity: OpAssociativity.RIGHT, arity: 2 },
-    "<": { code: OpCode.LT, precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 },
-    "<=": { code: OpCode.LTE, precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 },
-    ">": { code: OpCode.GT, precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 },
-    ">=": { code: OpCode.GTE, precedence: 0, associativity: OpAssociativity.LEFT, arity: 2 },
-    "=": { code: OpCode.EQ, precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "==": { code: OpCode.EQ, precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "!=": { code: OpCode.NEQ, precedence: -1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "&&": { code: OpCode.AND, precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "||": { code: OpCode.OR, precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "^^": { code: OpCode.XOR, precedence: -2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "~": { code: OpCode.NOT, precedence: 5, associativity: OpAssociativity.RIGHT, arity: 1 }, //prefix unary (L)
-    "!": { code: OpCode.FACT, precedence: 5, associativity: OpAssociativity.LEFT, arity: 1 }, //suffix unary (R)
-    "u-": { code: OpCode.NEG, precedence: 3, associativity: OpAssociativity.RIGHT, arity: 1 }, //prefix unary (L)
-    "times": { code: OpCode.CRP, precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 },
-    "\\": {code: OpCode.ATT, precedence: 101, associativity: OpAssociativity.LEFT, arity: 2},
-    "^n": { code: OpCode.POWN, precedence: 6, associativity: OpAssociativity.RIGHT, arity: 2 }, //for things like x^2!, the x^2 should come first as it is not x^{2!}
-    "_": { code: OpCode.SUBS, precedence: 6, associativity: OpAssociativity.RIGHT, arity: 2},
-    "±": { code: OpCode.PM, precedence: 1.5, associativity: OpAssociativity.LEFT, arity: 2 },
-    "pm": { code: OpCode.PM, precedence: 1, associativity: OpAssociativity.LEFT, arity: 2 },
-    "%": { code: OpCode.PCT, precedence: 6, associativity: OpAssociativity.LEFT, arity: 1 },
-    "degree": { code: OpCode.DEG, precedence: 6, associativity: OpAssociativity.LEFT, arity: 1},
-    "abs": { code: OpCode.ABS, precedence: 6, associativity: OpAssociativity.LEFT, arity: 1},
-    "times": { code: OpCode.CRP, precedence: 2, associativity: OpAssociativity.LEFT, arity: 2 },
+export const OperatorInfo = {
+    "+": { code: OperatorCode.ADD, precedence: 1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "-": { code: OperatorCode.SUB, precedence: 1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "*": { code: OperatorCode.MUL, precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "cdot": { code: OperatorCode.MUL, precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "/": { code: OperatorCode.DIV, precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "^": { code: OperatorCode.POW, precedence: 3, associativity: OperatorAssociativity.RIGHT, arity: 2 },
+    "<": { code: OperatorCode.LT, precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "<=": { code: OperatorCode.LTE, precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    ">": { code: OperatorCode.GT, precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    ">=": { code: OperatorCode.GTE, precedence: 0, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "=": { code: OperatorCode.EQ, precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "==": { code: OperatorCode.EQ, precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "!=": { code: OperatorCode.NEQ, precedence: -1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "&&": { code: OperatorCode.AND, precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "||": { code: OperatorCode.OR, precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "^^": { code: OperatorCode.XOR, precedence: -2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "~": { code: OperatorCode.NOT, precedence: 5, associativity: OperatorAssociativity.RIGHT, arity: 1 }, //prefix unary (L)
+    "!": { code: OperatorCode.FACT, precedence: 5, associativity: OperatorAssociativity.LEFT, arity: 1 }, //suffix unary (R)
+    "u-": { code: OperatorCode.NEG, precedence: 3, associativity: OperatorAssociativity.RIGHT, arity: 1 }, //prefix unary (L)
+    "times": { code: OperatorCode.CRP, precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "\\": {code: OperatorCode.ATT, precedence: 101, associativity: OperatorAssociativity.LEFT, arity: 2},
+    "^n": { code: OperatorCode.POWN, precedence: 6, associativity: OperatorAssociativity.RIGHT, arity: 2 }, //for things like x^2!, the x^2 should come first as it is not x^{2!}
+    "_": { code: OperatorCode.SUBS, precedence: 6, associativity: OperatorAssociativity.RIGHT, arity: 2},
+    "±": { code: OperatorCode.PM, precedence: 1.5, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "pm": { code: OperatorCode.PM, precedence: 1, associativity: OperatorAssociativity.LEFT, arity: 2 },
+    "%": { code: OperatorCode.PCT, precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1 },
+    "degree": { code: OperatorCode.DEG, precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1},
+    "abs": { code: OperatorCode.ABS, precedence: 6, associativity: OperatorAssociativity.LEFT, arity: 1},
+    "times": { code: OperatorCode.CRP, precedence: 2, associativity: OperatorAssociativity.LEFT, arity: 2 },
 }
 
